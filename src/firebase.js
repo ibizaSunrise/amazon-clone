aeabc747e929551e1951dcf97cf507b190972ae9
ibizaSunrise <milalemekh@gmail.com>
2020-12-10 10:31:53 +0100

    Set Up. Added firebase settings

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB3IS_8WtmymapbbVsQ7csmwPEeglIJmus",
+    authDomain: "challenge-e8cae.firebaseapp.com",
+    projectId: "challenge-e8cae",
+    storageBucket: "challenge-e8cae.appspot.com",
+    messagingSenderId: "131811080841",
+    appId: "1:131811080841:web:af8dfff2ca0866623aa458",
+    measurementId: "G-S96ER6ZSB5"
+  };
