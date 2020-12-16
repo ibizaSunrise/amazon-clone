7dd2dec3a114b59cc3b9a15593b4c0eba067dd5a
ibizaSunrise <milalemekh@gmail.com>
2020-12-16 12:20:50 +0100

    Added history, finished the user authentication

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,16 @@ import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import { Link } from 'react-router-dom';
 import {useStateValue} from './StateProvider';
+import { auth } from "./firebase";
 
 function Header() {
-    const [{basket}, dispatch] = useStateValue();
+    const [{basket, user}, dispatch] = useStateValue();
+
+    const handleAuthenticaton = () =>{
+        if (user) {
+            auth.signOut();
+        }
+    }
     return (
         <div className="header">
             <Link to='/'>
@@ -19,11 +26,11 @@ function Header() {
             </div>
 
             <div className="header__nav">
-                <Link to='/login'>
+                <Link to={!user && '/login'}>
 
-                <div className="header__option">
-                    <span className="headr__optionLineOne">Hello Guest</span>
-                    <span className="headr__optionLineTwo">Sign In</span>
+                <div className="header__option" onClick={handleAuthenticaton}>
+                    <span className="headr__optionLineOne">Hello {!user ? 'Guest' : user.email}</span>
+                    <span className="headr__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
                 </div>
                 </Link>
 
